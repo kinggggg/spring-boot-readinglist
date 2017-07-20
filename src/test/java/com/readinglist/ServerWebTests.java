5986ca1b37703988aeeaa78c54eeb2d2c83ebd7a
kinggggg <liweibooooo@126.com>
2017-07-20 09:54:06 +0800

    自动打开浏览器后，在地址栏并么有自动填充URL

--- src/test/java/com/readinglist/ServerWebTests.java
+++ src/test/java/com/readinglist/ServerWebTests.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.concurrent.TimeUnit;
@@ -14,44 +16,44 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes=ReadingListApplication.class)
-//@WebIntegrationTest(randomPort=true)
+@SpringBootTest
 public class ServerWebTests {
   
   private static FirefoxDriver browser;
-  
-  @Value("${local.server.port}")
-  private int port;
+
+  private static int port;
 
   @BeforeClass
   public static void openBrowser() {
+    System.out.println(port);
+
     browser = new FirefoxDriver();
     browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   }
   
   @Test
   public void addBookToEmptyList() {
-    String baseUrl = "http://localhost:" + port;
+    String baseUrl = "http://localhost:8080" ;
+    System.out.println(port);
     browser.get(baseUrl);
+    assertEquals("You have no books in your book list",browser.findElementByTagName("div").getText());
 
-    String currentUrl = browser.getCurrentUrl();
-    assertEquals(baseUrl +"/readingList", currentUrl);
-
-    assertEquals("You have no books in your book list", 
-                 browser.findElementByTagName("div").getText());
-
-    browser.findElementByName("title").sendKeys("BOOK TITLE");
-    browser.findElementByName("author").sendKeys("BOOK AUTHOR");
-    browser.findElementByName("isbn").sendKeys("1234567890");
-    browser.findElementByName("description").sendKeys("DESCRIPTION");
-    browser.findElementByTagName("form").submit();
-    
-    WebElement dl = 
-        browser.findElementByCssSelector("dt.bookHeadline");
-    assertEquals("BOOK TITLE by BOOK AUTHOR (ISBN: 1234567890)", 
-                 dl.getText());
-    WebElement dt = 
-        browser.findElementByCssSelector("dd.bookDescription");
+    browser.findElementByName("title")
+            .sendKeys("BOOK TITLE");
+    browser.findElementByName("author")
+            .sendKeys("BOOK AUTHOR");
+    browser.findElementByName("isbn")
+            .sendKeys("1234567890");
+    browser.findElementByName("description")
+            .sendKeys("DESCRIPTION");
+    browser.findElementByTagName("form")
+            .submit();
+    WebElement dl =
+            browser.findElementByCssSelector("dt.bookHeadline");
+    assertEquals("BOOK TITLE by BOOK AUTHOR (ISBN: 1234567890)",
+            dl.getText());
+    WebElement dt =
+            browser.findElementByCssSelector("dd.bookDescription");
     assertEquals("DESCRIPTION", dt.getText());
   }
   
