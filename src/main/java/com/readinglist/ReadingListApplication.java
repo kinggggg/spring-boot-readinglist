970e89609abe6a85c071c42d86b181ac214526c6
kinggggg <liweibooooo@126.com>
2017-09-20 10:34:00 +0800

    自定义自动化配置（未完成）

--- src/main/java/com/readinglist/ReadingListApplication.java
+++ src/main/java/com/readinglist/ReadingListApplication.java
@@ -1,12 +1,20 @@
 package com.readinglist;
 
+import com.readinglist.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ReadingListApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(ReadingListApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(ReadingListApplication.class, args);
+
+		HelloService helloService = ctx.getBean(HelloService.class);
+		System.out.println(helloService);
+
 	}
 }
